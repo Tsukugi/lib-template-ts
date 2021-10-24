c003f015285e4f3b436d547d8f962e1a4953c7ef
Tsukugi <TsukugiXIII@gmail.com>
2021-10-24 03:14:33 +0200

    First implementation of template
    Supports umd and commonjs2(modules)

---
+++ README.md
@@ -0,0 +1,4 @@
+# @atsu/template
+
+
+A really minimal implementation of a Typescript template that may be needed for a lib that exports both browser and node.
---
+++ webpack.helper.mjs
@@ -0,0 +1,84 @@
+import { resolve } from 'path';
+import nodeExternals from 'webpack-node-externals';
+
+const isObject = item => item && typeof item === 'object' && !Array.isArray(item);
+
+/* To deep merge a custom config with the default */
+const mergeDeep = (target, source) => {
+  let output = Object.assign({}, target);
+  if (isObject(target) && isObject(source)) {
+    Object.keys(source).forEach(key => {
+      if (isObject(source[key])) {
+        if (!(key in target)) Object.assign(output, { [key]: source[key] });
+        else output[key] = mergeDeep(target[key], source[key]);
+      } else {
+        Object.assign(output, { [key]: source[key] });
+      }
+    });
+  }
+  return output;
+};
+
+const defaultConfig = {
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts'],
+  },
+  externals: [nodeExternals()],
+};
+
+const getCustomTsLoaderOptions = options => {
+  return {
+    test: /\.tsx?$/,
+    use: [
+      {
+        loader: 'ts-loader',
+        options,
+      },
+    ],
+    exclude: /node_modules/,
+  };
+};
+
+const getConfigTemplate = config => {
+  const mergedConfig = mergeDeep(defaultConfig, { ...config });
+  console.log(mergedConfig);
+  return mergedConfig;
+};
+
+const cjsModules = ['commonjs', 'commonjs2'];
+const getOutput = ({ type, name }) => {
+  const ext = cjsModules.includes(type) ? 'cjs' : 'js';
+
+  return {
+    path: resolve('dist'),
+    filename: `impl.${name || type}.bundle.${ext}`,
+    library: { type },
+  };
+};
+
+const getResolveFallback = () => ({
+  fallback: { fs: false, http: false, https: false, url: false, path: false },
+});
+
+export {
+  isObject,
+  mergeDeep,
+  getResolveFallback,
+  getConfigTemplate,
+  getOutput,
+  defaultConfig,
+  getCustomTsLoaderOptions,
+};
